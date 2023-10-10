daa163c206956c7aa445ca21c481fc837d6dfccb
egg-silver <v6984@naver.com>
2023-10-10 21:12:14 +0900

    카드 레이아웃 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <SideBar />
       <Outlet />
