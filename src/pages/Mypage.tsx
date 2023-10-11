7f02f37afefd60177d096a34ab7dd29ae5141492
rladydqls99 <rladydqls99@naver.com>
2023-10-11 20:21:55 +0900

    마이페이지 인풋 퍼블리싱 완료

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,11 +1,25 @@
+import styled from 'styled-components';
+import InformationInput from '../components/mypage/InformationInput';
 import Profilepicture from '../components/mypage/Profilepicture';
 
 function Mypage() {
   return (
-    <div>
+    <Container>
       <Profilepicture />
-    </div>
+      <InformationInput />
+    </Container>
   );
 }
 
 export default Mypage;
+
+const Container = styled.div`
+  height: 65.0625rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  box-sizing: border-box;
+  padding: 11.0005rem 0rem 6.25rem 0rem;
+`;
