758617c7b23c27a3f9057b2f923893e544626134
rladydqls99 <rladydqls99@naver.com>
2023-10-26 19:44:25 +0900

    수정

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const baseInstance = axios.create({
   // withCredentials: true,
-  baseURL: 'http://43.200.8.55', // 기본 URL 설정
+  baseURL: 'https://api.chanyoungkang.com', // 기본 URL 설정
 });
 
 // https://api.chanyoungkang.com/
