fbe18a3318e64a4af112351f16563900619f8ba1
rladydqls99 <rladydqls99@naver.com>
2023-10-26 23:37:00 +0900

    주소 수정

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const baseInstance = axios.create({
   // withCredentials: true,
-  baseURL: 'http://43.200.8.55',
+  baseURL: 'https://api.chanyoungkang.com',
 });
 
 // https://api.chanyoungkang.com/
