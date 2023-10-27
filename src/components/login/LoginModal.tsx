5dda81bba36b23f1f33abddc7b5f1d481c627043
rladydqls99 <rladydqls99@naver.com>
2023-10-27 18:55:32 +0900

    상품 상태 토글 수정

--- src/components/login/LoginModal.tsx
+++ src/components/login/LoginModal.tsx
@@ -24,7 +24,7 @@ export default function LoginModal({ closeModal }: PropsType) {
           <Btn
             onClick={() => {
               window.location.href =
-                'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=851d6c649ed19d32afa2743c91134e57&redirect_uri=https://main-project-team-4-frontend.vercel.app/kakao/callback';
+                'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=851d6c649ed19d32afa2743c91134e57&redirect_uri=https://main-project-team-4-frontend.vercel.app/kakao';
             }}
           >
             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -41,7 +41,7 @@ export default function LoginModal({ closeModal }: PropsType) {
   );
 }
 // http://localhost:5173/kakao
-// https://main-project-team-4-frontend.vercel.app/kakao/callback
+// https://main-project-team-4-frontend.vercel.app/kakao
 const Layout = styled.div`
   display: flex;
   flex-direction: column;
