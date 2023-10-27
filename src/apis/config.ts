9b29ff30a2c8fc471b92c512b64db940d21125de
rladydqls99 <rladydqls99@naver.com>
2023-10-28 01:41:55 +0900

    test

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const baseInstance = axios.create({
-  baseURL: 'https://api.chanyoungkang.com',
+  baseURL: 'http://43.200.8.55',
 });
 
 // https://api.chanyoungkang.com
