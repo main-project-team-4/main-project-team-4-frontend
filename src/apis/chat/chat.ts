1a4760b5e1ceb8493c6145c156bfc3da154e6385
egg-silver <v6984@naver.com>
2023-10-30 22:32:33 +0900

    연결8

--- src/apis/chat/chat.ts
+++ src/apis/chat/chat.ts
@@ -1,28 +1,28 @@
 import { useEffect } from 'react';
 import SockJS from 'sockjs-client';
-// import { Stomp } from '@stomp/stompjs';
+import { Client } from '@stomp/stompjs';
 
 function WebSocketConnection() {
-  const Stomp = require('stompjs');
   useEffect(() => {
-    const sock = new SockJS('http://localhost:8080/ws-stomp');
-    const ws = Stomp.over(sock);
+    const client = new Client({
+      webSocketFactory: () => new SockJS('https://api.chanyoungkang.com/ws-stomp'),
+    });
 
-    ws.connect(
-      {},
-      (frame: any) => {
-        console.log('웹소켓 연결 성공:', frame);
-      },
-      (error: any) => {
-        console.error('웹소켓 연결 에러:', error);
-      },
-    );
+    client.onConnect = function (frame: any) {
+      console.log('웹소켓 연결 성공:', frame);
+    };
+
+    client.onStompError = function (error: any) {
+      console.error('웹소켓 연결 에러:', error.headers.message);
+    };
+
+    client.activate(); // 웹소켓 연결 시작
 
     // 컴포넌트가 언마운트될 때 실행될 로직
     return () => {
-      ws.disconnect(() => {
-        console.log('웹소켓 연결 종료');
-      });
+      client.deactivate();
+
+      console.log('웹소켓 연결 종료');
     };
   }, []);
 
