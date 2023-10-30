e1b8957c15fdd9c53d8a25298c23331bed6a483f
egg-silver <v6984@naver.com>
2023-10-30 17:23:51 +0900

    연결3

--- src/apis/chat/chat.ts
+++ src/apis/chat/chat.ts
@@ -1,18 +1,23 @@
 import { useEffect } from 'react';
 import SockJS from 'sockjs-client';
-import { Stomp } from '@stomp/stompjs';
 
-function WebSocketConnection() {
+// 타입 정의가 없다면 'any'로 대체
+// 만약 '@stomp/stompjs'의 타입 선언을 설치했다면 이 부분을 제거하거나 적절한 타입으로 대체하세요.
+declare module '@stomp/stompjs' {
+  export const Stomp: any;
+}
+
+function WebSocketConnection(): null {
   useEffect(() => {
     const sock = new SockJS('https://api.chanyoungkang.com/ws-stomp');
     const ws = Stomp.over(sock);
 
     ws.connect(
       {},
-      frame => {
+      (frame: any) => {
         console.log('웹소켓 연결 성공:', frame);
       },
-      error => {
+      (error: any) => {
         console.error('웹소켓 연결 에러:', error);
       },
     );
@@ -25,7 +30,7 @@ function WebSocketConnection() {
     };
   }, []);
 
-  return null; // UI는 없으며 연결만을 담당
+  return null;
 }
 
 export default WebSocketConnection;
