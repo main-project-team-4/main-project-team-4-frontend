1f676144f5fe8e3be78b1952a7de61351194c000
rladydqls99 <rladydqls99@naver.com>
2023-11-04 20:49:01 +0900

    recoil 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,20 @@
 import styled from 'styled-components';
 import CardLayout from '../components/layout/CardLayout';
 import { AllItems, TopItems, nearByItem } from '../apis/getItems/Item';
-import { useQueries, useQuery } from 'react-query';
+import { useQueries } from 'react-query';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { getMyInfo } from '../apis/mypage/members';
 import { getCookie } from '../utils/cookie';
 import { SyncLoader } from 'react-spinners';
+import { useRecoilValue } from 'recoil';
+import { myDataState } from '../Atoms';
 
 export default function Home() {
   const navigate = useNavigate();
   const token = getCookie('token');
   const { state } = useLocation();
 
-  const { data } = useQuery('myinfo', () => getMyInfo(token), {
-    enabled: !!token,
-  });
+  const data = useRecoilValue(myDataState);
 
   useEffect(() => {
     if (state) {
