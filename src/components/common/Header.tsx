59302d71e2d97372030005e6d3b91831f5bb6ff2
rladydqls99 <rladydqls99@naver.com>
2023-11-08 01:06:11 +0900

    svg 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -143,15 +143,15 @@ export default function Header() {
             ) : (
               <Btn onClick={openModal}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                  <g clip-path="url(#clip0_1414_3712)">
+                  <g clipPath="url(#clip0_1414_3712)">
                     <path
                       d="M12.5 6.66659V4.99992C12.5 4.55789 12.3244 4.13397 12.0118 3.82141C11.6993 3.50885 11.2754 3.33325 10.8333 3.33325H4.16667C3.72464 3.33325 3.30072 3.50885 2.98816 3.82141C2.67559 4.13397 2.5 4.55789 2.5 4.99992V14.9999C2.5 15.4419 2.67559 15.8659 2.98816 16.1784C3.30072 16.491 3.72464 16.6666 4.16667 16.6666H10.8333C11.2754 16.6666 11.6993 16.491 12.0118 16.1784C12.3244 15.8659 12.5 15.4419 12.5 14.9999V13.3333"
                       stroke="#0F172A"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
-                    <path d="M17.5003 10H6.66699M6.66699 10L9.16699 12.5M6.66699 10L9.16699 7.5" stroke="#0F172A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M17.5003 10H6.66699M6.66699 10L9.16699 12.5M6.66699 10L9.16699 7.5" stroke="#0F172A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                   </g>
                   <defs>
                     <clipPath id="clip0_1414_3712">
